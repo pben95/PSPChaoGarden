984ddc9a07941de08e1ef5bd3076e5d441ce97d6
pben95 <pben95@users.noreply.github.com>
2018-05-29 15:47:42 -0400

    Add files via upload

--- script.lua
+++ script.lua
@@ -1,12 +1,8 @@
 --480x272  240x136
-os.autofps(60)
-beige = color.new(255,218,185)
-green = color.new(80, 220, 100)
+--os.autofps(60)
 chaoStats = {"name", "age", "mood", "belly", "swim", "fly", "run", "power", "stamina", "rings"}
 items = {"swim", "fly", "run", "power", "stamina", "all", "none", "trumpet"}
 itemCosts = {["swim"] = 30, ["fly"] = 30, ["run"] = 30, ["power"]= 30, ["stamina"] = 40, ["all"] = 100, ["none"] = 100, ["trumpet"] = 500}
-buttonTimer = 0
-bellyTimer = 0
 images = {["egg"] = image.load("images/egg.png"),
 		  ["cursor"] = image.load("images/cursor.png"),
 		  ["grab"] = image.load("images/cursorGrab.png"),
@@ -35,12 +31,17 @@ chao = {["img"] = images["egg"],
 		["run"] = 0,
 		["power"] = 0,
 		["stamina"] = 0,
-		["rings"] = 1000
+		["rings"] = 1000,
+		["hunger"] = 0,
+		["move"] = 0,
+		["wx"] = 200,
+		["wy"] = 100
 }
 cursor = {["img"] = images["cursor"],
 		  ["x"] = 0,
 		  ["y"] = 0,
 		  ["held"] = nil,
+		  ["timer"] = 0
 }
 drawObjects = {cursor, chao}
 function useItem(item)
@@ -81,14 +82,58 @@ function buyItem(stat)
 		table.insert(drawObjects, item)
 	end
 end
-function gui()
-	image.blit(images["background"],0,0)
-	for i = 1, 10 do
-		screen.print(420, (i * 25) - 5, chao[chaoStats[i]])
+function cursorUse()
+	if cursor["held"] == nil then
+		if cursor["x"] < 30 then
+			for i = 1, 8 do
+				if (collide(11, cursor["x"], cursor["y"], 15, ((i*25)-5)) and #drawObjects < 15) then
+					buyItem(items[i])		
+				end
+			end
+		else
+			for i = 2, #drawObjects do
+				if collide(11, cursor["x"], cursor["y"], drawObjects[i]["x"], drawObjects[i]["y"]) then
+					cursor["img"] = images["grab"]
+					cursor["held"] = drawObjects[i]
+					drawObjects[i]["grabbed"] = true
+				end
+			end
+		end
+	elseif (cursor["held"] ~= nil and cursor["x"] > 29 and cursor["x"] < 398) then
+		cursor["held"]["grabbed"] = false
+		cursor["held"] = nil
+		cursor["img"] = images["cursor"]
 	end
-	draw.fillrect(0,0,30,272,beige)
-	for i = 1, 8 do
-		image.blit(images[items[i]], 10, (i*25)-5)
+end
+function chaoWander()
+	if (chao["grabbed"] == false and (os.clock() - chao["move"]) > 0.18) then
+		chao["move"] = os.clock()
+		if chao["x"] ~= chao["wx"] then
+			if chao["x"] < chao["wx"] then
+				chao["x"] = chao["x"] + 1
+			elseif chao["x"] > chao["wx"] then
+				chao["x"] = chao["x"] - 1
+			end
+		end
+		if chao["y"] ~= chao["wy"] then
+			if chao["y"] < chao["wy"] then
+				chao["y"] = chao["y"] + 1
+			elseif chao["y"] > chao["wy"] then
+				chao["y"] = chao["y"] - 1
+			end
+		end
+		for i = 3, #drawObjects do
+			if (collide(5, chao["x"], chao["y"], drawObjects[i]["x"], drawObjects[i]["y"]) and chao["belly"] < 9) then
+				useItem(drawObjects[i])
+			elseif (collide(50, chao["x"], chao["y"], drawObjects[i]["x"], drawObjects[i]["y"]) and chao["belly"] < 9) then
+				chao["wx"] = drawObjects[i]["x"]
+				chao["wy"] = drawObjects[i]["y"]
+			end
+		end
+		if (chao["x"] == chao["wx"] and chao["y"] == chao["wy"]) then
+			chao["wx"] = math.random(35, 390)
+			chao["wy"] = math.random(10, 260)
+		end
 	end
 end
 function collide(num, x1, y1, x2, y2)
@@ -98,22 +143,25 @@ function collide(num, x1, y1, x2, y2)
 		return false
 	end
 end
-function cursorGrab()
-	if cursor["x"] < 30 then
-		for i = 1, 8 do
-			if (collide(11, cursor["x"], cursor["y"], 15, ((i*25)-5)) and #drawObjects < 15) then
-				buyItem(items[i])		
-			end
-		end
-	else
-		for i = 2, #drawObjects do
-			if collide(11, cursor["x"], cursor["y"], drawObjects[i]["x"], drawObjects[i]["y"]) then
-				cursor["img"] = images["grab"]
-				cursor["held"] = drawObjects[i]
-				drawObjects[i]["grabbed"] = true
-			end
-		end
+function render()
+	screen.clear()
+	image.blit(images["background"],0,0)
+	for i = 1, 10 do
+		screen.print(420, (i * 25) - 5, chao[chaoStats[i]])
 	end
+	draw.fillrect(0,0,30,272,color.new(255,218,185))
+	for i = 1, 8 do
+		image.blit(images[items[i]], 10, (i*25)-5)
+	end
+	if cursor["held"] ~= nil then
+		cursor["held"]["x"] = cursor["x"] + 2
+		cursor["held"]["y"] = cursor["y"] + 13
+	end
+	for i = #drawObjects, 1, -1 do
+		image.blit(drawObjects[i]["img"],drawObjects[i]["x"],drawObjects[i]["y"])
+	end
+	screen.print(0,1,#drawObjects)
+	screen.flip()
 end
 function buttons()
 	controls.read()
@@ -127,18 +175,11 @@ function buttons()
 	elseif (controls.right() and cursor["x"] < 464) then
 		cursor["x"] = cursor["x"] + 2
 	end
-	if (controls.cross() and (os.clock() - buttonTimer) > 0.25) then
-		buttonTimer = os.clock()
-		if cursor["held"] == nil then
-			cursorGrab()
-		elseif (cursor["held"] ~= nil and cursor["x"] > 29 and cursor["x"] < 398) then
-			cursor["held"]["grabbed"] = false
-			cursor["held"] = nil
-			cursor["img"] = images["cursor"]
-		end
-	end
-	if (controls.square() and cursor["held"] ~= nil and chao["grabbed"] == false and collide(15, chao["x"], chao["y"], cursor["held"]["x"], cursor["held"]["y"]) and (os.clock() - buttonTimer) > 0.25) then
-		buttonTimer = os.clock()
+	if (controls.cross() and (os.clock() - cursor["timer"]) > 0.25) then
+		cursor["timer"] = os.clock()
+		cursorUse()
+	elseif (controls.square() and cursor["held"] ~= nil and chao["grabbed"] == false and collide(15, chao["x"], chao["y"], cursor["held"]["x"], cursor["held"]["y"]) and (os.clock() - cursor["timer"]) > 0.25) then
+		cursor["timer"] = os.clock()
 		useItem(cursor["held"])
 	end
 end
@@ -148,24 +189,13 @@ function game()
 	controls.waitforkey()
 	screen.clear()
 	while true do
-		screen.clear()
-		gui()
 		buttons()
-		if cursor["held"] ~= nil then
-			cursor["held"]["x"] = cursor["x"] + 2
-			cursor["held"]["y"] = cursor["y"] + 13
-		end
-		for i = #drawObjects, 1, -1 do
-			image.blit(drawObjects[i]["img"],drawObjects[i]["x"],drawObjects[i]["y"])
-		end
-		if (os.clock() - bellyTimer) > 10 then
-			bellyTimer = os.clock()
+		render()
+		if (os.clock() - chao["hunger"]) > 10 then
+			chao["hunger"] = os.clock()
 			chao["belly"] = chao["belly"] - 1
 		end
-		screen.print(0,0,#drawObjects)
-		screen.flip()
+		chaoWander()
 	end
 end
 game()
-
-

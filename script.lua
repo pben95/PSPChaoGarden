86b089adac56926de6bc81ed49c338a53d7586f1
pben95 <pben95@users.noreply.github.com>
2018-05-29 14:10:34 -0400

    Add files via upload

--- script.lua
+++ script.lua
@@ -3,9 +3,10 @@ os.autofps(60)
 beige = color.new(255,218,185)
 green = color.new(80, 220, 100)
 chaoStats = {"name", "age", "mood", "belly", "swim", "fly", "run", "power", "stamina", "rings"}
-itemStats = {"mood", "belly", "swim", "fly", "run", "power", "stamina", "all"}
-shopOn = false
+items = {"swim", "fly", "run", "power", "stamina", "all", "none", "trumpet"}
+itemCosts = {["swim"] = 30, ["fly"] = 30, ["run"] = 30, ["power"]= 30, ["stamina"] = 40, ["all"] = 100, ["none"] = 100, ["trumpet"] = 500}
 buttonTimer = 0
+bellyTimer = 0
 images = {["egg"] = image.load("images/egg.png"),
 		  ["cursor"] = image.load("images/cursor.png"),
 		  ["grab"] = image.load("images/cursorGrab.png"),
@@ -17,6 +18,8 @@ images = {["egg"] = image.load("images/egg.png"),
 		  ["power"] = image.load("images/power.png"),
 		  ["stamina"] = image.load("images/stamina.png"),
 		  ["all"] = image.load("images/all.png"),
+		  ["none"] = image.load("images/none.png"),
+		  ["trumpet"] = image.load("images/trumpet.png"),
 		  ["background"] = image.load("images/background.png")
 }
 chao = {["img"] = images["egg"],
@@ -25,14 +28,14 @@ chao = {["img"] = images["egg"],
 		["grabbed"] = false,
 		["name"] = "Chao",
 		["age"] = "Egg",
-		["mood"] = 0,
-		["belly"] = 0,
+		["mood"] = 10,
+		["belly"] = 10,
 		["swim"] = 0,
 		["fly"] = 0,
 		["run"] = 0,
 		["power"] = 0,
 		["stamina"] = 0,
-		["rings"] = 0
+		["rings"] = 1000
 }
 cursor = {["img"] = images["cursor"],
 		  ["x"] = 0,
@@ -40,33 +43,75 @@ cursor = {["img"] = images["cursor"],
 		  ["held"] = nil,
 }
 drawObjects = {cursor, chao}
-function newItem(stat)
-	local item = {["img"] = images[stat],
-			["x"] = 100,
-			["y"] = 50,
-			["grabbed"] = false,
-			["stat"] = stat
-	}
-	table.insert(drawObjects, item)
+function useItem(item)
+	if item["stat"] ~= "none" and item["stat"] ~= "trumpet" then
+		if chao["belly"] < 10 then
+			chao["belly"] = chao["belly"] + 1
+			chao["mood"] = chao["mood"] + 1
+			if item["stat"] == "all" then
+				for i = 5, 9 do
+					chao[chaoStats[i]] = chao[chaoStats[i]] + 2
+				end
+			else
+				chao[item["stat"]] = chao[item["stat"]] + 2
+			end
+		else
+			chao["mood"] = chao["mood"] - 1
+		end
+		cursor["held"] = nil
+		cursor["img"] = images["cursor"]
+		for i = 2, #drawObjects do
+			if drawObjects[i] == item then
+				table.remove(drawObjects, i)
+			end
+		end
+	end
+end
+function buyItem(stat)
+	if (chao["rings"] >= itemCosts[stat] and cursor["held"] == nil) then
+		chao["rings"] = chao["rings"] - itemCosts[stat]
+		local item = {["img"] = images[stat],
+				["x"] = cursor["x"] + 2,
+				["y"] = cursor["y"] + 13,
+				["grabbed"] = true,
+				["stat"] = stat
+		}
+		cursor["held"] = item
+		cursor["img"] = images["grab"]
+		table.insert(drawObjects, item)
+	end
 end
 function gui()
 	image.blit(images["background"],0,0)
 	for i = 1, 10 do
 		screen.print(420, (i * 25) - 5, chao[chaoStats[i]])
 	end
-	if shopOn == true then
-		draw.fillrect(0,0,30,272,beige)
-		for i = 3, 8 do
-			image.blit(images[itemStats[i]], 10, ((i-2)*25)-5)
-		end
+	draw.fillrect(0,0,30,272,beige)
+	for i = 1, 8 do
+		image.blit(images[items[i]], 10, (i*25)-5)
+	end
+end
+function collide(num, x1, y1, x2, y2)
+	if (math.abs(x1 - x2) < num and math.abs(y1 - y2) < num) then
+		return true
+	else
+		return false
 	end
 end
 function cursorGrab()
-	for i = 2, #drawObjects do
-		if (math.abs(cursor["x"] - drawObjects[i]["x"]) < 11 and math.abs(cursor["y"] - drawObjects[i]["y"]) < 11) then
-			cursor["img"] = images["grab"]
-			cursor["held"] = drawObjects[i]
-			drawObjects[i]["grabbed"] = true
+	if cursor["x"] < 30 then
+		for i = 1, 8 do
+			if (collide(11, cursor["x"], cursor["y"], 15, ((i*25)-5)) and #drawObjects < 15) then
+				buyItem(items[i])		
+			end
+		end
+	else
+		for i = 2, #drawObjects do
+			if collide(11, cursor["x"], cursor["y"], drawObjects[i]["x"], drawObjects[i]["y"]) then
+				cursor["img"] = images["grab"]
+				cursor["held"] = drawObjects[i]
+				drawObjects[i]["grabbed"] = true
+			end
 		end
 	end
 end
@@ -84,30 +129,17 @@ function buttons()
 	end
 	if (controls.cross() and (os.clock() - buttonTimer) > 0.25) then
 		buttonTimer = os.clock()
-		if (cursor["held"] == nil and shopOn == false) then
+		if cursor["held"] == nil then
 			cursorGrab()
-		elseif (cursor["held"] == nil and shopOn == true) then
-			for i = 1, 6 do
-				if (math.abs(cursor["x"] - 20) < 11 and math.abs(cursor["y"] - ((i*25)-5)) < 11 and #drawObjects < 15) then
-					newItem(itemStats[i+2])
-				end
-			end
-			if cursor["x"] > 31 then
-				shopOn = false
-			end
 		elseif (cursor["held"] ~= nil and cursor["x"] > 29 and cursor["x"] < 398) then
 			cursor["held"]["grabbed"] = false
 			cursor["held"] = nil
 			cursor["img"] = images["cursor"]
 		end
 	end
-	if (controls.l() and (os.clock() - buttonTimer) > 0.25) then
+	if (controls.square() and cursor["held"] ~= nil and chao["grabbed"] == false and collide(15, chao["x"], chao["y"], cursor["held"]["x"], cursor["held"]["y"]) and (os.clock() - buttonTimer) > 0.25) then
 		buttonTimer = os.clock()
-		if shopOn == true then
-			shopOn = false
-		else
-			shopOn = true
-		end
+		useItem(cursor["held"])
 	end
 end
 function game()
@@ -126,6 +158,11 @@ function game()
 		for i = #drawObjects, 1, -1 do
 			image.blit(drawObjects[i]["img"],drawObjects[i]["x"],drawObjects[i]["y"])
 		end
+		if (os.clock() - bellyTimer) > 10 then
+			bellyTimer = os.clock()
+			chao["belly"] = chao["belly"] - 1
+		end
+		screen.print(0,0,#drawObjects)
 		screen.flip()
 	end
 end
